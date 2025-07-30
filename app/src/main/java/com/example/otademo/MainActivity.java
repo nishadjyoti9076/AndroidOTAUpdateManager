4f88d889433f94bd0cbbc60a7052421c361da68c
jyotinishad9076 <jaggujyoti1999@gmail.com>
2025-07-30 16:26:08 +0530

    worked in build updegration and rollback to specific version

--- app/src/main/java/com/example/otademo/MainActivity.java
+++ app/src/main/java/com/example/otademo/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.otademo;
 
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +25,34 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        PackageManager pm = getPackageManager();
+        PackageInfo pInfo;
+
+        try {
+            pInfo = pm.getPackageInfo(getPackageName(), 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        String currentVersion = pInfo.versionName;
+        TextView tv = findViewById(R.id.tvVersionDetails);
+        tv.setText("Version : "+currentVersion);
+
+        Button btnNext = findViewById(R.id.btnNext);
+        btnNext.setOnClickListener(v -> nextToDashboard());
+
+        Button btnCheckUpdate = findViewById(R.id.btnCheckUpdate);
+        btnCheckUpdate.setOnClickListener(v -> checkForUpdate());
+    }
+
+    public void nextToDashboard(){
+        Intent intent=new Intent(this,DashboardActivity.class);
+        startActivity(intent);
+    }
+
+    public void checkForUpdate(){
+        Intent intent = new Intent(this, DownloadInstallFile.class);
+        startActivity(intent);
     }
 }
